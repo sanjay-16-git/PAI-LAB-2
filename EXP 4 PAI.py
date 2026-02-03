37677a2185aace5c7f4e8db2f64a2d78809d00b9
sanjay-16-git <rajasanjayjakkam655@gmail.com>
2026-02-03 12:02:18 +0530

    Add files via upload

---
+++ EXP 4 PAI.py
@@ -0,0 +1,41 @@
+
+def print_solution(board):
+    for row in board:
+        print(" ".join("Q" if col else "." for col in row))
+    print("\n")
+def is_safe(board, row, col, n):
+    for i in range(row):
+        if board[i][col] == 1:
+            return False
+    i, j = row, col
+    while i >= 0 and j >= 0:
+        if board[i][j] == 1:
+            return False
+        i -= 1
+        j -= 1
+    i, j = row, col
+    while i >= 0 and j < n:
+        if board[i][j] == 1:
+            return False
+        i -= 1
+        j += 1
+    return True
+def solve_nqueens(board, row, n):
+    if row == n:
+        print_solution(board)
+        return True
+    res = False
+    for col in range(n):
+        if is_safe(board, row, col, n):
+            board[row][col] = 1
+            res = solve_nqueens(board, row + 1, n) or res
+            board[row][col] = 0 
+    return res
+def nqueens(n=8):
+    board = [[0 for _ in range(n)] for _ in range(n)]
+    if not solve_nqueens(board, 0, n):
+        print("No solution exists")
+
+
+
+nqueens(8)
